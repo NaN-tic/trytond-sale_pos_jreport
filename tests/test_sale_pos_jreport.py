b1fbd8e86e4ba9b125f41ffa6c9c1ff84e329b73
resteve <resteve@zikzakmedia.com>
2015-12-01 11:21:19 +0100

    Tests

--- tests/test_sale_pos_jreport.py
+++ tests/test_sale_pos_jreport.py
@@ -1,28 +1,18 @@
-#!/usr/bin/env python
-# This file is part sale_pos_jreport module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the sale_pos_jreport module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SalePOSJreportTestCase(unittest.TestCase):
-    'Test Sale POS Jasper Report module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_pos_jreport')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class SalePosJreportTestCase(ModuleTestCase):
+    'Test Sale Pos Jreport module'
+    module = 'sale_pos_jreport'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        SalePOSJreportTestCase))
+        SalePosJreportTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
