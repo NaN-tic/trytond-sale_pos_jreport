9ef143a4e3c3355ed87dabce7fa1ff523cf67c8e
resteve <resteve@zikzakmedia.com>
2016-01-27 19:20:31 +0100

    Pagination (merge pdf) when print > 1 records

--- sale.py
+++ sale.py
@@ -10,3 +10,17 @@ __metaclass__ = PoolMeta
 
 class SaleTicketReport(JasperReport):
     __name__ = 'sale_pos.sale_ticket'
+
+    @classmethod
+    def execute(cls, ids, data):
+        if len(ids) > 1:
+            pdfs = []
+            for id in ids:
+                report = super(SaleTicketReport, cls).execute([id], data)
+                pdfs.append(report[1])
+                dprint = report[1]
+
+            merged = cls.merge_pdfs(pdfs)
+            return ('pdf', bytearray(merged), dprint, 'sale_ticket')
+        else:
+            return super(SaleTicketReport, cls).execute(ids, data)
